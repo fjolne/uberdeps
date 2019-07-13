9e96aa3cdf7508f1ab86d47836812a0ca5aa1504
fjolne <fjolne.yngling@gmail.com>
2019-07-13 16:50:49 +0300

    Ignore IDEA files, removed Lein files, fixed deps.edn

--- project.clj
+++
@@ -1,7 +0,0 @@
-(defproject uberdeps "0.1.4"
-  :description "Uberjar builder for deps.edn"
-  :license     {:name "MIT" :url "https://github.com/tonsky/uberdeps/blob/master/LICENSE"}
-  :url         "https://github.com/tonsky/uberdeps"
-  :dependencies
-  [[org.clojure/clojure "1.10.1-RC1"]
-   [org.clojure/tools.deps.alpha "0.6.496"]])
